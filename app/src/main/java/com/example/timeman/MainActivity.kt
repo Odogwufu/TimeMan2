bc22d7bb7591c55777df792d59cf2c11fd14915f
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-07-08 16:18:19 -0400

    Adding Timer Functionality 1

--- app/src/main/java/com/example/timeman/MainActivity.kt
+++ app/src/main/java/com/example/timeman/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.timeman
 
-
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import com.example.timeman.databinding.ActivityMainBinding
 import kotlin.math.roundToInt
 
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+        val button =findViewById<Button>(R.id.secondActivityBtn)
+        button.setOnClickListener{
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
 }
-
-
-
-
