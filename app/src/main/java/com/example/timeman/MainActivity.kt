1c75eb6f28e3e8e0d8620d834a8c425cb63ff5b7
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2022-07-23 18:52:43 -0400

    added working to do list

--- app/src/main/java/com/example/timeman/MainActivity.kt
+++ app/src/main/java/com/example/timeman/MainActivity.kt
@@ -1,54 +1,59 @@
 package com.example.timeman
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.timeman.R
-import com.example.timeman.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private var items: ArrayList<String>? = null
-    private var itemsAdapter: ArrayAdapter<String>? = null
-    private var listView: ListView? = null
+
+    private lateinit var items: ArrayList<String>
+    private lateinit var itemsAdapter: ArrayAdapter<String>
+    private lateinit var button: Button
+    private lateinit var listView:ListView
+
+
 
     override fun onCreate(SavedInstanceState: Bundle?) {
         super.onCreate(SavedInstanceState)
         setContentView(R.layout.activity_main)
-        listView = findViewById(R.id.listView)
-        binding.button.setOnClickListener { view -> addItem(view) }
+
+        listView =findViewById(R.id.listView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener(this::addItem)
         items = ArrayList()
         itemsAdapter = ArrayAdapter(
             this, android.R.layout.simple_list_item_1,
-            items!!
+            items
         )
-        listView?.adapter = itemsAdapter
+        listView.adapter =itemsAdapter
         setupListViewListener()
     }
 
     private fun setupListViewListener() {
-        listView!!.onItemLongClickListener =
-            OnItemLongClickListener { parent: AdapterView<*>?, view: View?, i: Int, l: Long ->
-                val context = applicationContext
-                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show()
-                items!!.removeAt(i)
-                itemsAdapter!!.notifyDataSetChanged()
-                true
-            }
+        listView.setOnItemLongClickListener{parent:AdapterView<*>?,view:View?,i,l ->
+
+            val context : Context = applicationContext
+            Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show()
+            items.removeAt(i)
+            itemsAdapter.notifyDataSetChanged()
+            true
+        }
     }
 
-    private fun addItem(view: View) {
+    private fun addItem(view:View) {
         val input = findViewById<EditText>(R.id.editText2)
         val itemText = input.text.toString()
         if (itemText != "") {
-            itemsAdapter!!.add(itemText)
+            itemsAdapter.add(itemText)
             input.setText("")
         } else {
             Toast.makeText(applicationContext, "PLease enter text...", Toast.LENGTH_LONG).show()
         }
     }
 }
-
-
