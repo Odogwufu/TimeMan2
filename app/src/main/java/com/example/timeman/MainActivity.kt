0dfe08ae57a785b11a0b49d1daad992662845965
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-07-29 14:00:37 -0400

    Added firebase real time database

--- app/src/main/java/com/example/timeman/MainActivity.kt
+++ app/src/main/java/com/example/timeman/MainActivity.kt
@@ -5,9 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var itemsAdapter: ArrayAdapter<String>
     private lateinit var button: Button
     private lateinit var listView:ListView
-
+    var firebaseDatabase: FirebaseDatabase? = null
+    var databaseReference: DatabaseReference? = null
 
 
     override fun onCreate(SavedInstanceState: Bundle?) {
@@ -40,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         )
         listView.adapter =itemsAdapter
         setupListViewListener()
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase!!.getReference("EmployeeInfo");
     }
 
     private fun setupListViewListener() {
@@ -59,8 +63,36 @@ class MainActivity : AppCompatActivity() {
         if (itemText != "") {
             itemsAdapter.add(itemText)
             input.setText("")
+            addDatatoFirebase(itemText);
         } else {
             Toast.makeText(applicationContext, "PLease enter text...", Toast.LENGTH_LONG).show()
         }
+    }
+    private fun addDatatoFirebase(itemText: String) {
+        // below 3 lines of code is used to set
+        // data in our object class.
+        items.add(itemText)
+
+
+        // we are use add value event listener method
+        // which is called with database reference.
+        databaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // inside the method of on Data change we are setting
+                // our object class to our database reference.
+                // data base reference will sends data to firebase.
+                databaseReference!!.setValue(items)
+
+                // after adding this data we are showing toast message.
+                Toast.makeText(this@MainActivity, "data added", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // if the data is not added or it is cancelled then
+                // we are displaying a failure toast message.
+                Toast.makeText(this@MainActivity, "Fail to add data $error", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 }
