2282e9afb8e43edbd270937495f2884e599508e4
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-07-31 17:14:49 -0400

    Trial

---
+++ app/src/main/java/com/example/timeman/dto/Profile.kt
@@ -0,0 +1,6 @@
+package com.example.timeman.dto
+
+data class Profile (val username : String, val profiledes : String?) {
+
+}
+
---
+++ app/src/test/java/com/example/timeman/Testing_Task_list.kt
@@ -0,0 +1,46 @@
+package com.example.timeman
+
+import org.junit.Assert
+import org.junit.Test
+
+class Testing_Task_list {
+
+    lateinit var mainActivity: MainActivity
+
+
+    var allitems  = ArrayList<String>()
+
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+
+    var items= ArrayList<String>()
+
+    @Test
+    suspend fun Task_being_added_to_list(item : String) {
+
+        MainActivityInitialized()
+        ListData()
+        thenlistShouldContainfire()
+    }
+    private fun MainActivityInitialized() {
+        mainActivity = MainActivity()
+    }
+
+    private suspend fun ListData() {
+        var allitems = mainActivity.fetchItems()
+    }
+
+    private fun thenlistShouldContainfire() {
+        Assert.assertNotNull(allitems)
+        Assert.assertTrue(allitems!!.isNotEmpty())
+        var containsfire = false
+        allitems!!.forEach {
+            if (items.contains("fire")){
+                containsfire = true
+            }
+        }
+        Assert.assertTrue(containsfire)
+    }
+}
