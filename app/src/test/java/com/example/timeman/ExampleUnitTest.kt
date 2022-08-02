54b56d9ad1470bf911047ee1dc79385280c65937
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-08-02 15:54:08 -0400

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/timeman/ExampleUnitTest.kt
+++ app/src/test/java/com/example/timeman/ExampleUnitTest.kt
@@ -3,9 +3,6 @@ package com.example.timeman
 import org.junit.Test
 
 import org.junit.Assert.*
-import org.junit.Rule
-import org.junit.rules.TestRule
-
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,47 +10,68 @@ import org.junit.rules.TestRule
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-
-//    @get:Rule
-//    var rule: TestRule = InstantTaskExecutorRule()
-
-    lateinit var mainActivity: MainActivity
-
-
-  var allitems  = ArrayList<String>()
-
-    private fun mainactivityinitialized() {
-        mainActivity = MainActivity()
-    }
-
-
-    var items= ArrayList<String>()
-
     @Test
-    suspend fun Task_being_added_to_list(item : String) {
-
-        MainActivityInitialized()
-        ListData()
-        thenlistShouldContainfire()
-    }
-    private fun MainActivityInitialized() {
-        mainActivity = MainActivity()
-    }
-
-    private suspend fun ListData() {
-        var allitems = mainActivity.fetchItems()
-    }
-
-    private fun thenlistShouldContainfire() {
-        assertNotNull(allitems)
-        assertTrue(allitems!!.isNotEmpty())
-        var containsfire = false
-        allitems!!.forEach {
-            if (items.contains("fire")){
-                containsfire = true
-            }
-        }
-        assertTrue(containsfire)
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
+
+// package com.example.timeman
+
+// import org.junit.Test
+
+// import org.junit.Assert.*
+// import org.junit.Rule
+// import org.junit.rules.TestRule
+
+
+// /**
+//  * Example local unit test, which will execute on the development machine (host).
+//  *
+//  * See [testing documentation](http://d.android.com/tools/testing).
+//  */
+// class ExampleUnitTest {
+
+// //    @get:Rule
+// //    var rule: TestRule = InstantTaskExecutorRule()
+
+//     lateinit var mainActivity: MainActivity
+
+
+//   var allitems  = ArrayList<String>()
+
+//     private fun mainactivityinitialized() {
+//         mainActivity = MainActivity()
+//     }
+
+
+//     var items= ArrayList<String>()
+
+//     @Test
+//     suspend fun Task_being_added_to_list(item : String) {
+
+//         MainActivityInitialized()
+//         ListData()
+//         thenlistShouldContainfire()
+//     }
+//     private fun MainActivityInitialized() {
+//         mainActivity = MainActivity()
+//     }
+
+//     private suspend fun ListData() {
+//         var allitems = mainActivity.fetchItems()
+//     }
+
+//     private fun thenlistShouldContainfire() {
+//         assertNotNull(allitems)
+//         assertTrue(allitems!!.isNotEmpty())
+//         var containsfire = false
+//         allitems!!.forEach {
+//             if (items.contains("fire")){
+//                 containsfire = true
+//             }
+//         }
+//         assertTrue(containsfire)
+//     }
+// }
 
